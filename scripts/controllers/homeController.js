6b3cdb326343904d0c5458d34a295dd51824325e
SolarFloss <nicholasmrnickmorris@gmail.com>
2017-12-22 10:41:47 -0500

    getting closer I can feel it

--- scripts/controllers/homeController.js
+++ scripts/controllers/homeController.js
@@ -4,7 +4,7 @@ angular.module('main').controller('homeController',function($scope,$rootScope){
 
     $scope.keyPressed = function(e){
         if(e.key == "Enter"){
-            $rootScope.socket = io('localhost:3000');
+            $rootScope.socket = io('https://mysterious-hollows-74939.herokuapp.com');
             
             //Request to play with user with specified id
             $rootScope.socket.emit('request',$scope.input);
@@ -60,7 +60,7 @@ angular.module('main').controller('homeController',function($scope,$rootScope){
 
 
     $rootScope.newGame = function(){
-        $rootScope.socket = io('localhost:3000');
+        $rootScope.socket = io('http://mysterious-hollows-74939.herokuapp.com');
         
         $rootScope.socket.on('get-id',function(data){
             $rootScope.id = data;

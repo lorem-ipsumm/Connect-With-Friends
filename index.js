21fed849c6ed73fa79a3262231a9f4f578426e86
SolarFloss <nicholasmrnickmorris@gmail.com>
2017-12-22 01:03:33 -0500

    whoops

--- index.js
+++ index.js
@@ -37,6 +37,6 @@ io.on('connection',function(socket){
     });
 });
 
-server..listen(process.env.PORT || 5000,function(){
+server.listen(process.env.PORT || 5000,function(){
   console.log("listening on PORT: " + process.env.PORT);
 });

f8d616aa4be6302e967af3bf75fdb2efcc3126c4
SolarFloss <nicholasmrnickmorris@gmail.com>
2017-12-24 23:02:15 -0500

    I styled some things up

--- scripts/controllers/homeController.js
+++ scripts/controllers/homeController.js
@@ -5,38 +5,49 @@ angular.module('main').controller('homeController',function($scope,$rootScope){
     $scope.message = document.getElementsByClassName('info-message')[0];
     
 
+
+
+    $scope.guestSetup = function(){
+        console.log("h");
+        $rootScope.socket = io('https://connect-with-friends.herokuapp.com', {secure: true, rejectUnauthorized: false});
+        
+        //Request to play with user with specified id
+        $rootScope.socket.emit('request',$scope.input);
+        
+        //Is called when host responds to guest
+        $rootScope.socket.on('request',function(data){
+            $rootScope.playing = true;
+            $rootScope.requested = true;
+            $rootScope.id = data[1];
+            //If the data is an array, setup match
+            //[hostId,guestId,hostTurn,color]
+            if(data[0] != false){
+                $rootScope.friend = data[0];
+                if(data[2] == true)
+                    $rootScope.turn = false;
+                else
+                    $rootScope.turn = true;
+
+                if(data[3] == 1)
+                    $rootScope.color = 'yellow';
+                else
+                    $rootScope.color = 'red';
+                
+                
+                document.location.href = "../#!/game";
+            }else if(data[0] == false){
+                $scope.message.innerHTML = "It Looks Like Your Friend Is Busy :/";
+            }
+        });
+    };
+
+    $scope.startClicked = function(){
+        $scope.guestSetup();
+    };
+
     $scope.keyPressed = function(e){
         if(e.key == "Enter"){
-            $rootScope.socket = io('https://connect-with-friends.herokuapp.com', {secure: true, rejectUnauthorized: false});
-            
-            //Request to play with user with specified id
-            $rootScope.socket.emit('request',$scope.input);
-            
-            //Is called when host responds to guest
-            $rootScope.socket.on('request',function(data){
-                $rootScope.playing = true;
-                $rootScope.requested = true;
-                $rootScope.id = data[1];
-                //If the data is an array, setup match
-                //[hostId,guestId,hostTurn,color]
-                if(data[0] != false){
-                    $rootScope.friend = data[0];
-                    if(data[2] == true)
-                        $rootScope.turn = false;
-                    else
-                        $rootScope.turn = true;
-
-                    if(data[3] == 1)
-                        $rootScope.color = 'yellow';
-                    else
-                        $rootScope.color = 'red';
-                    
-                    
-                    document.location.href = "../#!/game";
-                }else if(data[0] == false){
-                    $scope.message.innerHTML = "It Looks Like Your Friend Is Busy :/";
-                }
-            });    
+            $scope.guestSetup();
         }
     }
 
